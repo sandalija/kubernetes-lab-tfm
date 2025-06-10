40fe8ecf23c05a75522f9b192af0c75c155b2d21
Sergio <sergiocava.d@gmail.com>
2025-06-10 06:31:54 +0200

    Add missing examples + fix Ansible

--- run.sh
+++ run.sh
@@ -3,13 +3,20 @@
 ###############################
 # Terraform
 ###############################
+echo "cd terraform/"
 cd terraform/
+
+echo "terraform init -backend-config remote-state.tfbackend"
 terraform init -backend-config remote-state.tfbackend
+
+echo "terraform apply"
 terraform apply
 
 # Extract the IP address from Terraform output
+echo "new_ip=\$(terraform output -raw bastion_ip)"
 new_ip=$(terraform output -raw bastion_ip)
 
+echo "cd -"
 cd -
 
 ###############################
@@ -29,7 +36,7 @@ if ! grep -q "Host bastion" "$ssh_config"; then
 fi
 
 # Use sed to replace the HostName line under 'Host bastion'
-# Note: This assumes HostName is on its own line under Host bastion
+echo "sed -i.bak -E \"/Host bastion/,/Host / s/(HostName )[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/\\1\$new_ip/\" \"\$ssh_config\""
 sed -i.bak -E "/Host bastion/,/Host / s/(HostName )[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/\1$new_ip/" "$ssh_config"
 
 echo "Updated 'bastion' IP to $new_ip in $ssh_config"
@@ -37,6 +44,25 @@ echo "Updated 'bastion' IP to $new_ip in $ssh_config"
 ###############################
 # Ansible
 ###############################
+echo "cd ansible"
 cd ansible
-ansible-playbook main.yaml -i inventory.yaml
 
+if [ ! -d ".venv" ]; then
+  echo "python3 -m venv .venv"
+  python3 -m venv .venv
+fi
+
+echo "source .venv/bin/activate"
+source .venv/bin/activate
+
+echo "pip install -r requirements.txt"
+pip install -r requirements.txt
+
+echo "ansible-galaxy collection install -r requirements.yaml"
+ansible-galaxy collection install -r requirements.yaml
+
+echo "ansible-playbook main.yaml"
+ansible-playbook main.yaml
+
+echo "ssh bastion"
+ssh bastion
